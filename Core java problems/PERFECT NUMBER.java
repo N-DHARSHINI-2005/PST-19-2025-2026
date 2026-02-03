6fea6c97adc0b3752644068e58481afbee6fa936
Dharshini N <dharshini20050916@gmail.com>
2026-02-03 20:18:45 +0530

    Add files via upload

---
+++ Core java problems/PERFECT NUMBER.java
@@ -0,0 +1,27 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.In)
+	    
+	for(int i=1; i<n; i++)
+	{
+	    if (n%i==0)
+	    {
+	        tempt =i;
+	    }
+	}
+	if(tempt==n)
+	}
+	{
+	    System.out.printin
+	}
+}
