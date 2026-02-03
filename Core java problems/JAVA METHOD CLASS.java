2fc7309f3de3bd0db23170d4851ed115eebdcb77
Dharshini N <dharshini20050916@gmail.com>
2026-02-03 20:18:06 +0530

    Add files via upload

---
+++ Core java problems/JAVA METHOD CLASS.java
@@ -0,0 +1,29 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+public class Main {
+    print all();
+	public static void print all() {
+		Dharani();
+		Dharshini();
+		Deeksha();
+		Monisha();
+		
+	}
+		public static void Dharani() {
+        System.out.println("Dharani:she is beatiful grils");
+		}
+		public static void Dharshini() {
+        System.out.println("Dharshini:she is beatiful grils");
+		}
+		public static void Deeksha() {
+        System.out.println("Deeksha:she is beatiful grils");
+		}
+		public static void Monisha() {
+        System.out.println("Monisha:she is beatiful grils");
+	}
+}
